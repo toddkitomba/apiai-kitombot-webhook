755201bc3e8d2f6fd1735116964417d7c7aebe73
Paul Sinclair <p4ul.sincl4ir@gmail.com>
2017-06-22 15:58:41 +1200

    added graph test

--- intents/reporting.py
+++ intents/reporting.py
@@ -1,16 +1,19 @@
 from __future__ import print_function
 from future.standard_library import install_aliases
 import requests
+
 install_aliases()  # not sure what this does...
 
-
+import plotly.plotly as py
+from plotly.graph_objs import *
 
 url = "https://staging.kitomba.com/k1/clients_ajax/get_client_appointments_info_for_day/Today/93895ccd5153edb2f7b09193b9225f89/all"
 token = "8623a3ff07832d2fe4d7079aa811745d"
 headers = {'Token': token}
+py.sign_in('paul.sinclair', '20xGivFuRABOeYkaCyTq')
 
 
-def test():
+def test2():
     response = requests.post(url, None, True, verify=False, headers=headers)
     result = response.json()
     print(result)
@@ -26,6 +29,33 @@ def test():
 
     }
 
+
+def test():
+    trace0 = Scatter(
+        x=[1, 2, 3, 4],
+        y=[10, 15, 13, 17]
+    )
+    trace1 = Scatter(
+        x=[1, 2, 3, 4],
+        y=[16, 5, 11, 9]
+    )
+    data = Data([trace0, trace1])
+
+    url = py.plot(data, filename='basic-line')
+
+    text = "hi"
+    return {
+        "speech": text,
+        "displayText": text,
+        "attachments": [
+            {
+                "contentType": "image/png",
+                "contentUrl": url + ".png",
+                "name": url + ".png"
+            }
+        ]
+
+    }
 
 
 if __name__ == '__main__':
