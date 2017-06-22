d1aed486677994d12b5a9b152c8b05c8d53973d2
Paul Sinclair <p4ul.sincl4ir@gmail.com>
2017-06-22 14:51:51 +1200

    redirected, thanks todd

---
+++ intents/reporting.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from future.standard_library import install_aliases
+import requests
+install_aliases()  # not sure what this does...
+
+
+
+url = "https://staging.kitomba.com/k1/clients_ajax/get_client_appointments_info_for_day/Today/93895ccd5153edb2f7b09193b9225f89/all"
+token = "8623a3ff07832d2fe4d7079aa811745d"
+headers = {'Token': token}
+
+
+def test():
+    response = requests.post(url, None, True, verify=False, headers=headers)
+    result = response.json()
+    print(result)
+
+    text = result['models'][0]['first_name']
+    text += result['models'][0]['start_date']
+    text += result['models'][0]['appt_status']
+
+    return {
+        "speech": text,
+        "displayText": text,
+
+    }
+
+
+
+if __name__ == '__main__':
+    print(test())
