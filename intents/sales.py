ec231f438c1b76fa84221766e47d199c4e8f1408
Paul Sinclair <p4ul.sincl4ir@gmail.com>
2017-06-23 12:38:31 +1200

    got sales working, did some refactoring

--- intents/sales.py
+++ intents/sales.py
@@ -1,23 +1,36 @@
 from __future__ import print_function
+
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont, ImageSequence
+
 from future.standard_library import install_aliases
 import requests
 
 install_aliases()  # not sure what this does...
 
-
+import pprint
 
 # k1 api example
-def today(base_url, token):
-    url = base_url + "/k1/clients_ajax/get_client_appointments_info_for_day/Today/93895ccd5153edb2f7b09193b9225f89/all"
+def today(base_url, token, business_token):
+
+    url = base_url + "/k1/dashboard_ajax/getDashboardData?bid="+ business_token +"&byStaff=false&date=2017-06-23+10%3A37%3A49&GST=exclusive&stage=db_stage_1"
+
+    print(url)
+
     headers = {'Token': token}
     response = requests.post(url, None, True, verify=False, headers=headers)
     result = response.json()
-    print(result)
+    pprint.pprint(result.get('data').get('sales').get('by_time').get('so_far').get('total'))
 
-    text = "First visit of today\n"
-    text += result['models'][0]['first_name'] + "\n"
-    text += result['models'][0]['start_date'] + "\n"
-    text += result['models'][0]['appt_status']
+
+
+
+    text = "Sales so far "
+    text += "$" + str(result.get('data').get('sales').get('by_time').get('so_far').get('total'))
+    text += "(expected $" + str(result.get('data').get('sales').get('by_time').get('expected').get('total')) + ")"
+    # text += result['models'][0]['first_name'] + "\n"
+    # text += result['models'][0]['start_date'] + "\n"
+    # text += result['models'][0]['appt_status']
 
     return {
         "speech": text,
---
+++ intents/weather.py
@@ -0,0 +1,76 @@
+# noinspection PyCompatibility
+from urllib.parse import urlencode
+# noinspection PyCompatibility
+from urllib.request import urlopen
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+
+def doYahooWeatherForecast(req):
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+# noinspection SqlDialectInspection
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    print("data")
+    print(json.dumps(data, indent=4))
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
